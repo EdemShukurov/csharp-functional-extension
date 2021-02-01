6081ed2c32ded395f5709af86b24815ae024a91c
Edem Shukurov <edem96@mail.ru>
2021-02-01 12:21:39 +0500

    Update Bind.cs

--- FunctionalExtension/Bind.cs
+++ FunctionalExtension/Bind.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FunctionalExtension.Common;
+using System;
 
 namespace FunctionalExtension
 {
@@ -6,12 +7,35 @@ namespace FunctionalExtension
     {
         public static R Bind<T,R>(Func<T, R> nextFunction, T input)
         {
-            if(input == null)
-            {
-                return default(R);
-            }
+            return input == null
+                        ? default
+                        : nextFunction(input);
+        }
 
-            return nextFunction(input);
+        public static Result<R> Bind<T, R>(Func<Result<T>, Result<R>> nextFunction, Result<T> input)
+        {
+            return input.IsSuccess 
+                        ? nextFunction(input)
+                        : Result<R>.Fail(input.Error);
+        }
+
+        public static R? Bind<T, R>(this T? self, Func<T, R?> binder)
+            where T : struct
+            where R : struct
+        {
+            return self.HasValue
+                        ? binder(self.Value)
+                        : default(R);
+        }
+
+
+        public static R? Bind<T, R>(this T? self, Func<T, R?> Some, Func<R?> None)
+            where T : struct
+            where R : struct
+        {
+            return self.HasValue
+                        ? Some(self.Value)
+                        : None();
         }
     }
 }
